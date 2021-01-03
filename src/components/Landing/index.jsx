6e67dfbd1646741c1e723b0a6740dfc03260d46f
isox <theo.meunier41@gmail.com>
2021-01-03 15:06:03 +0100

    feat:
    mise en place des boutons

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -1,12 +1,35 @@
 import React from "react";
+import {useRef, useEffect, useState, Fragment} from "react";
 
 export default function Landing () {
-    return <main className="welcomePage">
+
+    const [btn, setBtn] = useState(false)
+
+    const refWolverine = useRef(null)
+
+    useEffect(() => {
+        refWolverine.current.classList.add("startingImg");
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg");
+            setBtn(true)
+        }, 1000)
+    }, [])
+
+    const displayBtn = btn && (
+        <>
             <div className="leftBox">
                 <button className="btn-welcome">Inscription</button>
             </div>
             <div className="rightBox">
                 <button className="btn-welcome">Conexion</button>
             </div>
+        </>
+
+    )
+
+
+
+    return <main ref={refWolverine} className="welcomePage">
+        {displayBtn}
     </main>
 }
