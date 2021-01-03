5aa79f9f7c30b05156550a5172bafd62980de59d
isox <theo.meunier41@gmail.com>
2021-01-03 15:40:47 +0100

    feat:
    Les évènements onMouseOver et onMouseOut

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -13,21 +13,36 @@ export default function Landing () {
             refWolverine.current.classList.remove("startingImg");
             setBtn(true)
         }, 1000)
-    }, [])
+    }, []);
+
+    const setLeftImg = () => {
+        refWolverine.current.classList.add("leftImg");
+    }
+
+    const setRightImg = () => {
+        refWolverine.current.classList.add("rightImg");
+    }
+
+    const clearImg = () => {
+        if (refWolverine.current.classList.contains("leftImg")) {
+            refWolverine.current.classList.remove("leftImg");
+        }
+        else if (refWolverine.current.classList.contains("rightImg")){
+            refWolverine.current.classList.remove("rightImg");
+        }
+    }
 
     const displayBtn = btn && (
-        <>
-            <div className="leftBox">
+        <Fragment>
+            <div onMouseOver={setLeftImg} onMouseOut={clearImg} className="leftBox">
                 <button className="btn-welcome">Inscription</button>
             </div>
-            <div className="rightBox">
-                <button className="btn-welcome">Conexion</button>
+            <div onMouseOver={setRightImg} onMouseOut={clearImg} className="rightBox">
+                <button  className="btn-welcome">Conexion</button>
             </div>
-        </>
+        </Fragment>
 
     )
-
-
 
     return <main ref={refWolverine} className="welcomePage">
         {displayBtn}
